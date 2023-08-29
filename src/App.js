ade872ad69d39f2ed65c42379442a27b3125ea5c
oommpphh <86844201+oommpphh@users.noreply.github.com>
2023-08-29 18:07:46 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
